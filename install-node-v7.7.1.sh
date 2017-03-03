01c327a198c97d7de0218c17e1aa2d4c4909b824
Steven de Salas <sdesalas@users.noreply.github.com>
2017-03-03 12:03:37 +1100

    Adding install-node-v7.7.1.sh

---
+++ install-node-v7.7.1.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# By Steven de Salas
+
+# Based on script by Richard Stanley @ https://github.com/audstanley/Node-MongoDb-Pi/
+# This is for a RaspberryPi Zero but should work across all models.
+
+VERSION=v7.7.1;
+
+# Creates directory for downloads, and downloads node
+cd ~/ && mkdir temp && cd temp;
+wget https://nodejs.org/dist/$VERSION/node-$VERSION-linux-armv6l.tar.gz;
+tar -xzf node-$VERSION-linux-armv6l.tar.gz;
+# Remove the tar after extracing it.
+sudo rm node-$VERSION-linux-armv6l.tar.gz;
+# This line will clear existing nodejs
+sudo rm -rf /opt/nodejs;
+# This next line will copy Node over to the appropriate folder.
+sudo mv node-$VERSION-linux-armv6l /opt/nodejs/;
+# Remove existing symlinks
+sudo unlink /usr/bin/node;
+sudo unlink /usr/sbin/node;
+sudo unlink /sbin/node;
+sudo unlink /usr/local/bin/node;
+sudo unlink /usr/bin/npm;
+sudo unlink /usr/sbin/npm;
+sudo unlink /sbin/npm;
+sudo unlink /usr/local/bin/npm;
+# Create symlinks to node && npm
+sudo ln -s /opt/nodejs/bin/node /usr/bin/node;
+sudo ln -s /opt/nodejs/bin/node /usr/sbin/node;
+sudo ln -s /opt/nodejs/bin/node /sbin/node;
+sudo ln -s /opt/nodejs/bin/node /usr/local/bin/node;
+sudo ln -s /opt/nodejs/bin/npm /usr/bin/npm;
+sudo ln -s /opt/nodejs/bin/npm /usr/sbin/npm;
+sudo ln -s /opt/nodejs/bin/npm /sbin/npm;
+sudo ln -s /opt/nodejs/bin/npm /usr/local/bin/npm;
+
+
