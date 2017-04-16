c95a0a48c767f8514585b8d17f52cc882eb8409b
David Gray <grayda@solidinc.org>
2017-04-17 02:29:57 +1000

    Added URL generation

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+// Change this to match your GitHub username
+var username = "grayda"
+var architecture = "armv6l"
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var fs = require("fs")
+var async = require("async")
+
+var http = require('http')
+var GetNodeVersions = require("get-node-versions").GetNodeVersions
+
+gulp.task('generate', function() {
+  var validVersions = []
+  var template = fs.readFileSync("install-node-template.txt", "utf8")
+  fs.writeFileSync("README.md", fs.readFileSync("readme-template.txt", "utf8"))
+  gutil.log("Requesting all node versions..")
+  GetNodeVersions.parse(["all"]).then(function(versions) {
+    async.map(versions, function(version, next) {
+      options = {method: 'HEAD', host: "nodejs.org", port: 80, path: '/dist/v' + version + "/node-v" + version + "-linux-armv6l.tar.gz"}
+      req = http.request(options, function(r, err) {
+        if(r.statusCode == "200") {
+          gutil.log(gutil.colors.green("v" + version + " has an armv61 build. Writing install script.."))
+          validVersions.push(version)
+          next(null, version)
+        } else {
+          gutil.log(gutil.colors.red("v" + version + " has no armv61 build. Skipping.."))
+          next(null, null)
+        }
+      });
+      req.end();
+    }, function() {
+      validVersions.sort().forEach(function(version) {
+        gutil.log("Writing " + version + " to file")
+        fs.writeFile("./install-node-v" + version + ".sh", template.replace("@@VERSION@@", version).replace("@@ARCH@@", architecture).replace("@@MIRROR@@", GetNodeVersions.NODEJS_MIRROR))
+        fs.appendFileSync("README.md", ["## v" + version, "", "```sh", "$ wget -O - https://raw.githubusercontent.com/" + username + "/node-pi-zero/master/install-node-v" + version + ".sh", "```", ""].join("\n"))
+      })
+      gutil.log(gutil.colors.cyan("Wrote " + validVersions.length + " versions"))
+    })
+  })
+})
---
+++ readme-template.txt
@@ -0,0 +1,20 @@
+![Node Pi Zero](https://raw.githubusercontent.com/sdesalas/node-pi-zero/eb55d80c724950137b79dbeded540789dc3cf455/pizero.png)
+
+# node-pi-zero
+
+Use the commands below to install node in the Raspberry Pi Zero.
+
+They are re-runnable so you can upgrade or downgrade just by running the command for each version.
+
+When finished just check the node and npm versions.
+
+```sh
+$ node -v
+$ npm -v
+```
+
+And remember, [friends don't let friends pipe to sh](https://www.seancassidy.me/dont-pipe-to-your-shell.html).
+
+## Re-generating this list
+
+To regenerate this list, clone this repo, run `npm install` to install dependencies, then `gulp generate` to recreate these files
