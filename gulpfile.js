f317f580fb8a5b9ee6ce2333fd270f9561d051a6
David Gray <grayda@solidinc.org>
2017-04-17 02:32:43 +1000

    Include forgotten | bash pipe

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('generate', function() {
       validVersions.sort().forEach(function(version) {
         gutil.log("Writing " + version + " to file")
         fs.writeFile("./install-node-v" + version + ".sh", template.replace("@@VERSION@@", version).replace("@@ARCH@@", architecture).replace("@@MIRROR@@", GetNodeVersions.NODEJS_MIRROR))
-        fs.appendFileSync("README.md", ["## v" + version, "", "```sh", "$ wget -O - https://raw.githubusercontent.com/" + username + "/node-pi-zero/master/install-node-v" + version + ".sh", "```", ""].join("\n"))
+        fs.appendFileSync("README.md", ["## v" + version, "", "```sh", "$ wget -O - https://raw.githubusercontent.com/" + username + "/node-pi-zero/master/install-node-v" + version + ".sh | bash", "```", ""].join("\n"))
       })
       gutil.log(gutil.colors.cyan("Wrote " + validVersions.length + " versions"))
     })
