d1af4cbc351ef3cd1ac8233df1e4632780d15862
Kees C. Bakker <KeesCBakker@users.noreply.github.com>
2017-04-20 20:34:40 +0200

    Create install-node-v-last.sh
    Finds the latest node version, downloads and install it.

---
+++ install-node-v-last.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# By Steven de Sales
+
+# 2017-04-20 Getting latest NodeJs version by Kees C. Bakker (KeesTalksTech)
+# Based on script by Richard Stanley @ https://github.com/audstanley/Node-MongoDb-Pi/
+
+#get pi ARM version
+PI_ARM_VERSION=arm$(cat /proc/cpuinfo | egrep "v[0-9]l" -o);
+
+#get latest nodejs version from node website
+VERSION=$(curl https://nodejs.org/dist/ |
+egrep '>v([0-9]\.?){3}' -o |
+tail -1 |
+cut -c 2-
+);
+
+# Creates directory for downloads, and downloads node
+cd ~/ && mkdir temp && cd temp;
+wget https://nodejs.org/dist/$VERSION/node-$VERSION-linux-$PI_ARM_VERSION.tar.gz;
+tar -xzf node-$VERSION-linux-$PI_ARM_VERSION.tar.gz;
+
+# Remove the tar after extracing it.
+sudo rm node-$VERSION-linux-$PI_ARM_VERSION.tar.gz;
+
+# This line will clear existing nodejs
+sudo rm -rf /opt/nodejs;
+
+# This next line will copy Node over to the appropriate folder.
+sudo mv node-$VERSION-linux-$PI_ARM_VERSION /opt/nodejs/;
+
+# Remove existing symlinks
+sudo unlink /usr/bin/node;
+sudo unlink /usr/sbin/node;
+sudo unlink /sbin/node;
+sudo unlink /usr/local/bin/node;
+sudo unlink /usr/bin/npm;
+sudo unlink /usr/sbin/npm;
+sudo unlink /sbin/npm;
+sudo unlink /usr/local/bin/npm;
+
+# Create symlinks to node && npm
+sudo ln -s /opt/nodejs/bin/node /usr/bin/node;
+sudo ln -s /opt/nodejs/bin/node /usr/sbin/node;
+sudo ln -s /opt/nodejs/bin/node /sbin/node;
+sudo ln -s /opt/nodejs/bin/node /usr/local/bin/node;
+sudo ln -s /opt/nodejs/bin/npm /usr/bin/npm;
+sudo ln -s /opt/nodejs/bin/npm /usr/sbin/npm;
+sudo ln -s /opt/nodejs/bin/npm /sbin/npm;
+sudo ln -s /opt/nodejs/bin/npm /usr/local/bin/npm;
