f1963a264013cd9a3b90774c1e25efcd4bc51d85
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-11-13 10:31:43 +0700

    what!

--- index.js
+++ index.js
@@ -4,7 +4,103 @@ const fs = require('fs');
 let where = ""
 const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY || "";
 
-fetch('https://unofficial-builds.nodejs.org/download/release/index.json')
+async function get_request() {
+  let array = []
+  const url = 'https://nodejs.org/dist/index.json'
+  const res = await fetch(url);
+  const data = await res.json();//assuming data is json
+  data.forEach(function (item) {
+    //if item.files array has linux-armv6l
+    if (item.files.includes("linux-armv6l")) {
+      console.log(item.version)
+      try {
+        if (fs.existsSync('./install-node-' + item.version + '.sh')) {
+          console.log('File exists');
+        } else {
+          let content = ''
+          try {
+            const data = fs.readFileSync('./install-node-v.example_official.sh', 'utf8')
+            console.log(data)
+            content = data.replace('{{version}}', item.version)
+          } catch (err) {
+            console.error(err)
+          }
+          fs.writeFile('./install-node-' + item.version + '.sh', content, err => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            //file written successfully
+          })
+        }
+        array.push(item.version.substring(1))
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  })
+
+  const unofficialurl = 'https://unofficial-builds.nodejs.org/download/release/index.json'
+  const unofficialres = await fetch(unofficialurl);
+  const unofficialdata = await unofficialres.json();//assuming data is json
+  unofficialdata.forEach(function (item) {
+    if (item.files.includes("linux-armv6l")) {
+      console.log(item.version)
+      try {
+        if (fs.existsSync('./install-node-' + item.version + '.sh')) {
+          console.log('File exists');
+        } else {
+          let content = ''
+          try {
+            const data = fs.readFileSync('./install-node-v.example.sh', 'utf8')
+            console.log(data)
+            content = data.replace('{{version}}', item.version)
+          } catch (err) {
+            console.error(err)
+          }
+          fs.writeFile('./install-node-' + item.version + '.sh', content, err => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            //file written successfully
+          })
+        }
+        array.push(item.version.substring(1))
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  })
+  return array
+  //console.log(data)
+}
+
+get_request().then(function (data) {
+  wow = data.map(a => a.split('.').map(n => +n + 100000).join('.')).sort().map(a => a.split('.').map(n => +n - 100000).join('.'));
+
+  wow.forEach(element => {
+    where += "\n## v" + element + "\n```sh\n$ wget -O - https://raw.githubusercontent.com/" + GITHUB_REPOSITORY + "/master/install-node-v" + element + ".sh | bash\n```\n"
+  });
+
+  let haha
+  try {
+    const data = fs.readFileSync('./README_example.md', 'utf8')
+    console.log(data)
+    haha = data.replace('{{testlink}}', where)
+  } catch (err) {
+    console.error(err)
+  }
+  fs.writeFile('./README.md', haha, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    //file written successfully
+  })
+})
+
+/*fetch('https://unofficial-builds.nodejs.org/download/release/index.json')
   .then(res => res.json())
   .then(json => {
     json.forEach(element => {
@@ -47,10 +143,10 @@ fetch('https://unofficial-builds.nodejs.org/download/release/index.json')
       }
     });
 
-    wow = wow.map( a => a.split('.').map( n => +n+100000 ).join('.') ).sort().map( a => a.split('.').map( n => +n-100000 ).join('.') );
+    wow = wow.map(a => a.split('.').map(n => +n + 100000).join('.')).sort().map(a => a.split('.').map(n => +n - 100000).join('.'));
 
     wow.forEach(element => {
-      where += "\n## v" + element + "\n```sh\n$ wget -O - https://raw.githubusercontent.com/"+GITHUB_REPOSITORY+"/master/install-node-v" + element + ".sh | bash\n```\n"
+      where += "\n## v" + element + "\n```sh\n$ wget -O - https://raw.githubusercontent.com/" + GITHUB_REPOSITORY + "/master/install-node-v" + element + ".sh | bash\n```\n"
     });
 
     let haha
@@ -68,4 +164,4 @@ fetch('https://unofficial-builds.nodejs.org/download/release/index.json')
       }
       //file written successfully
     })
-  });
+  });*/
